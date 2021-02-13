a8d083a204a97d1687a713565f97ddf96e76535d
Kemar Meeks <59718677+meeks21@users.noreply.github.com>
2021-02-12 22:52:46 -0800

    assignment 6

--- clubApp/urls.py
+++ clubApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('meetingDetail/<int:id>', views.meetingDetail, name='detail'),
     path('meetings/', views.meetings, name='meetings'),
     path('resources/', views.resources, name='resources'),
 ]
--- clubApp/views.py
+++ clubApp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMin, Resource, Event
+from django.urls import reverse_lazy
+
 # Create your views here.
 def index (request):
     return render(request, 'clubApp/index.html')
@@ -7,6 +9,10 @@ def index (request):
 def meetings(request):
     meeting_list=Meeting.objects.all()
     return render(request, 'clubApp/meetings.html', {'meeting_list' : meeting_list})
+
+def meetingDetail(request, id):
+    meeting=get_object_or_404(Meeting, pk=id)
+    return render(request, 'clubApp/meeting_detail.html', {'meeting' : meeting})
 
 def resources(request):
     resource_list=Resource.objects.all()
