c3f06d317078948c862663107339770aff265e49
Kemar Meeks <59718677+meeks21@users.noreply.github.com>
2021-02-19 00:41:44 -0800

    deletes txt file

--- clubApp/tests.py
+++ clubApp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Meeting, MeetingMin, Resource, Event
+
 
 # Create your tests here.
+class MeetingTest(TestCase):
+    def setUp(self):
+        self.type=Meeting(meetingname='Intro to Python Club')
+    
+    def test_type(self):
+        self.assertEqual(str(self.type), 'Intro to Python Club')
